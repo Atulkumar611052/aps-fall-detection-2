775297f23603651c28575e269aea030237de618b
Atul <anamikagolu1508@gmail.com>
2022-12-18 10:18:42 +0530

    update

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
 import sys,os
+from sensor.entity.config_entity import DataIngestionConfig
 
-def test_test_logger_exceptiom():
-     try:
-          result = 3/0  
-          print(result)   
-     except Exception as e:
-          raise SensorException (e,sys)
 
 
 if __name__=="__main__":
      try:
-          test_test_logger_exceptiom()
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
      except Exception as e:
           print(e)
      
