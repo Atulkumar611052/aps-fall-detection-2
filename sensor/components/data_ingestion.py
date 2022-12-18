b22f52c1393e8d132c562cde40865169788dbf0a
Atul <anamikagolu1508@gmail.com>
2022-12-18 11:17:14 +0530

    update

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -20,23 +20,52 @@ class DataIngestion:
 
     def initiate_data_ingestion(self)-> artifact_entity.DataIngestionArtifact:
         try:
+            logging.info(f"Exporting collection data as pandas dataframe")
             #Exporting collection data
             df:DataFrame = utils.get_collection_as_dataframe(
                 database_name=self.data_ingestion_config.database_name,
                  collection_name=self.data_ingestion_config.collection_name)
-
+            logging.info("Save Data in feature Store")
             # Save Data in feature Store
             df.replace(to_replace="na",value=np.NAN,inplace=True)
 
-            # Create Feature Store Folder
-            self.data_ingestion_config.feature_store_file_path
+            logging.info("Create Feature Store Folder if not available")     
+            # Create Feature Store Folder if not available
+            feature_store_dir= os.path.dirname(self.data_ingestion_config.feature_store_file_path)
             os.makedirs(feature_store_dir,exist_ok=True)
-
+            
+            logging.info(" Save df to Feature Store Folder")
             # Save df to Feature Store Folder
             df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path,index= False,headre=True)
-
+            
+            logging.info("split dataset into train and test")
             # split dataset into train and test
             train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size)
+
+            logging.info("create dataset directory folder if not available")
+            # create dataset directory folder if not available
+            dataset_dir=os.path.dirname(self.data_ingestion_config.train_file_path)
+            os.makedirs(dataset_dir,exist_ok=True)
+
+            logging.info("save df to feature store folder")
+            #save df to feature store folder
+            df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path,index=False,header=True)
+
+            logging.info("Save df to feature store folder")
+            #Save df to feature store folder
+            train_df.to_csv(path_or_buf=self.data_ingestion_config.train_file_path,index=False,header=True)
+            test_df.to_csv(path_or_buf=self.data_ingestion_config.test_file_path,index=False,header=True)
+
+              #Prepare artifact
+
+            data_ingestion_artifact = artifact_entity.DataIngestionArtifact(
+                feature_store_file_path=self.data_ingestion_config.feature_store_file_path,
+                train_file_path=self.data_ingestion_config.train_file_path, 
+                test_file_path=self.data_ingestion_config.test_file_path)
+
+            logging.info(f"Data ingestion artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
+
 
         except Exception as e:
             raise SensorException(error_message=e, error_detail=sys)
