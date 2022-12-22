f5e1d98bf620c1b681dcadf379612acef9d8f89b
Atul <anamikagolu1508@gmnail.com>
2022-12-22 22:17:24 +0530

    update

--- sensor/components/model_trainer.py
+++ sensor/components/model_trainer.py
@@ -4,7 +4,8 @@ from sensor.exception import SensorException
 import os,sys
 from typing import Optional
 from xgboost import XGBClassifier
-
+from sensor import utils
+from sklearn.metrics import f1_score
 
 class ModelTrainer:
 
@@ -17,11 +18,30 @@ class ModelTrainer:
         except Exception as e:
             raise e
 
-    @property
-    def model(self):
+    
+    def train_model(self,x,y):
+        xgb_clf=XGBClassifier()
+        xgb_clf.fit(x,y)
+        return xgb_clf
 
-     def initiate_model_trainer(self,)->artifact_entity.ModelTrainerArtifact:
+    def initiate_model_trainer(self,)->artifact_entity.ModelTrainerArtifact:
         try:
-            pass
+            train_arr=utils.load_numpy_array_data(file_path=sself.data_transformation_artifact.transformed_train_path)
+            test_arr=utils.load_numpy_array_data(file_path=sself.data_transformation_artifact.transformed_test_path)
+
+            x_train,y_train=train_arr[:,:-1],train_arr[:,-1]
+            x_test,y_test=test_arr[:,:-1],test_arr[:,-1]
+
+
+            model=train_model(x=x_train,y=y_train)
+
+            yhat_train=model.predict(x_train)
+            f1_train_score=f1_score(y_true=y_train,y_pred=yhat_train)
+
+            yhat_test=model.predict(x_test)
+            f1_test_score=f1_score(y_true=y_test,y_pred=yhat_testy)
+            
+            # check for overfitting or underfitting or expected score
+            
         except Exception as e:
             raise e
