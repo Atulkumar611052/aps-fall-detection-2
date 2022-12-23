e1ec0e5f110dbe2d1ffbb1b8cdc85ce1c2d66351
Atul <anamikagolu1508@gmail.com>
2022-12-23 07:19:52 +0530

    update model trainer

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys,os
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.components import data_ingestion
 from sensor.components.data_validation import DataValidation
-
+from sensor.components.model_trainer import ModelTrainer
 
 print(__name__)
 if __name__=="__main__":
@@ -13,14 +13,27 @@ if __name__=="__main__":
           training_pipeline_config = config_entity.TrainingPipelineConfig()
           data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
+          # Dat ingestion
           data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
           data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
-          
+          # Data validation
           data_validation_config=config.entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
           data_validation=DataValidation(data_validation_config=data_validation_config,
                           data_ingestion_artifact=data_ingestion_artifact)
 
-          data_validation_artifact=data_validation.initiate_data_validation()          
+          data_validation_artifact=data_validation.initiate_data_validation()
+          
+          # Data transformation
+          data_transformation_config=config_entity.data_transformation_config(training_pipeline_config=training_pipeline_config),
+          data_transformation=DataTransformation(data_transformation_config=data_transformation_config,
+          data_ingestion_artifact=data_ingestion_artifact)
+          data_transformation_artifact = data_transformation.initiate_data_transformation()
+
+          # Model trainer
+          model_trainer_config=config_entity.ModelTrainer(training_pipeline_config=training_pipeline_config)
+          model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+          model_trainer_artifact = model_trainer.initiate_model_trainer()
+
      except Exception as e:
           print(e)
      
